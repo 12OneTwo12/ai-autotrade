8be193a34325c43579ed9e3a352b86ef497bdc5a
12OneTwo12 <jji042842@gmail.com>
2025-07-30 09:36:24 +0900

    default project setting

---
+++ src/main/kotlin/me/onetwo/aiautotrade/AiAutoTradeApplication.kt
@@ -0,0 +1,11 @@
+package me.onetwo.aiautotrade
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiAutoTradeApplication
+
+fun main(args: Array<String>) {
+    runApplication<AiAutoTradeApplication>(*args)
+}
---
+++ src/test/kotlin/me/onetwo/aiautotrade/AiAutoTradeApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.onetwo.aiautotrade
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AiAutoTradeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
