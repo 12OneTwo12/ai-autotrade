cfc979df585da32ce20f6dcf42a7e834ac041b51
12OneTwo12 <jji042842@gmail.com>
2025-08-07 10:39:01 +0900

    feat: JOOQ 의존성 추가
    Closes gh-12

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,8 @@ plugins {
 
     id("jacoco")
     id ("io.gitlab.arturbosch.detekt") version "1.23.6"
+
+    id("org.jooq.jooq-codegen-gradle") version "3.20.1"
 }
 
 group = "me.onetwo"
@@ -42,6 +44,17 @@ dependencies {
     testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
     testImplementation("org.springframework.security:spring-security-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // JOOQ 기본 런타임 라이브러리
+    implementation("org.jooq:jooq:3.20.1")
+
+    implementation("org.springframework.boot:spring-boot-starter-jooq") // jooq, Spring과 통합하는 경우 추가
+
+    // JOOQ 코드 생성 관련 라이브러리
+    jooqCodegen("org.jooq:jooq-meta:3.20.1")
+    jooqCodegen("org.jooq:jooq-codegen:3.20.1")
+    // DDLDatabase 지원 라이브러리 추가
+    jooqCodegen("org.jooq:jooq-meta-extensions:3.20.1")
 }
 
 kotlin {
@@ -165,4 +178,46 @@ detekt {
     allRules = false
     config.setFrom(file(detektConfigFile)) // Detekt에서 제공된 yml에서 Rule 설정 On/Off 가능
     ignoreFailures = true // detekt 빌드시 실패 ignore 처리
+}
+
+jooq {
+    configuration {
+        generator {
+            name = "org.jooq.codegen.KotlinGenerator"
+
+            database {
+                name = "org.jooq.meta.extensions.ddl.DDLDatabase" // DDL 파일 사용 설정
+                properties {
+                    property {
+                        key = "scripts"
+                        value = "src/main/resources/sql/create-table-sql.sql" // DDL 파일 경로
+                    }
+                    property {
+                        key = "sort"
+                        value = "semantic" // 테이블 참조 관계 정렬
+                    }
+                    property {
+                        key = "unqualifiedSchema"
+                        value = "true" // 스키마 이름 제거
+                    }
+                }
+            }
+            generate {
+                isPojos = true  // Kotlin 데이터 클래스로 생성
+                isDaos = true   // DAO 클래스 생성
+            }
+            target {
+                packageName = "com.onetwo.aiautotrade.jooq.generated" // JOOQ 코드 저장 경로
+                directory = "src/main/generated"
+            }
+        }
+    }
+}
+
+sourceSets {
+    main {
+        kotlin {
+            srcDirs("src/main/generated") // JOOQ 코드가 있는 폴더 소스 코드로 추가
+        }
+    }
 }
